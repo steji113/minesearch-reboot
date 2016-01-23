10d99889e99d1a6d5572313b784a113f7053edcc
mattmemmesheimer <m.memmesheimer@gmail.com>
2016-01-23 11:49:37 -0600

    Adding Prism and Unity.

---
+++ MineSearch/MineSearch.Wpf/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+
+namespace MineSearch.Wpf
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            Container.RegisterType<MainWindow>();
+        }
+
+        /// <see cref="UnityBootstrapper.InitializeModules" />
+        protected override void InitializeModules()
+        {
+            base.InitializeModules();
+            Application.Current.MainWindow = Container.Resolve<MainWindow>();
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
--- MineSearch/MineSearch.Wpf/MainWindow.xaml.cs
+++ MineSearch/MineSearch.Wpf/MainWindow.xaml.cs
@@ -1,37 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using MineSearch.Common;
-using MineSearch.Game;
-
-namespace MineSearch.Wpf
+﻿namespace MineSearch.Wpf
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
-
-            // Create some simple game settings
-            var gameSettings = new GameSettings(2, 2, 1);
-            // Use the default random number generator
-            IPointGenerator rndGen = new RandomPointGenerator(2, 2);
-            var cellFactory = new MineSearchCellsFactory(gameSettings, rndGen);
-            var game = new MineSearchGame(cellFactory);
         }
     }
 }
