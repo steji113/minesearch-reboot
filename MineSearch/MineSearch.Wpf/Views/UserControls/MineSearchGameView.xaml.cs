325580563b7a8c6836778fd47222e8cf799b0314
mattmemmesheimer <m.memmesheimer@gmail.com>
2016-01-23 15:49:40 -0600

    Starting mine search game view.

---
+++ MineSearch/MineSearch.Wpf/Views/UserControls/MineSearchGameView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MineSearch.Wpf.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MineSearchGameView.xaml
+    /// </summary>
+    public partial class MineSearchGameView
+    {
+        public MineSearchGameView()
+        {
+            InitializeComponent();
+        }
+    }
+}
