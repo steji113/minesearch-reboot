47d292b3ca4ac97f97374deb1d3b30e3fc8ab97d
mattmemmesheimer <m.memmesheimer@gmail.com>
2016-01-26 17:05:52 -0600

    Improving the cell view.

---
+++ MineSearch/MineSearch.Wpf/Converters/AdjacentMineCountToStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MineSearch.Wpf.Converters
+{
+    public class AdjacentMineCountToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int val = (int)value;
+            if (val == 0)
+            {
+                return "";
+            }
+            return string.Format("{0}", val);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
