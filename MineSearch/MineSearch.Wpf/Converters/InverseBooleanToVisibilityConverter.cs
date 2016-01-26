985d23e3bef1eca2a9755490e7ebd2a5006d2be5
mattmemmesheimer <m.memmesheimer@gmail.com>
2016-01-26 17:40:39 -0600

    Adding comments.

--- MineSearch/MineSearch.Wpf/Converters/InverseBooleanToVisibilityConverter.cs
+++ MineSearch/MineSearch.Wpf/Converters/InverseBooleanToVisibilityConverter.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Globalization;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
 
 namespace MineSearch.Wpf.Converters
 {
+    /// <summary>
+    /// Represents the converter that converts <see cref="Boolean"/> values to and from
+    /// <see cref="Visibility"/> enumeration values.
+    /// </summary>
+    /// <remarks>
+    /// This class is the inverse of <see cref="BooleanToVisibilityConverter"/>.
+    /// </remarks>
     public class InverseBooleanToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
