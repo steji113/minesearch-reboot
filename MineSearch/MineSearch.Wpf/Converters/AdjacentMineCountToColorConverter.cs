1ebd74959c0b2a68ff18ccfa5aa6f36b94c7065a
mattmemmesheimer <m.memmesheimer@gmail.com>
2016-01-29 19:18:35 -0600

    Adding color convertsion to adjacent mine count.  Increasing size of inner mine ellipse.

---
+++ MineSearch/MineSearch.Wpf/Converters/AdjacentMineCountToColorConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MineSearch.Wpf.Converters
+{
+    public class AdjacentMineCountToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int mineCount = (int) value;
+            Color color;
+            switch (mineCount)
+            {
+                case 1:
+                    color = Colors.Blue;
+                    break;
+                case 2:
+                    color = Colors.Green;
+                    break;
+                case 3:
+                    color = Colors.Red;
+                    break;
+                case 4:
+                    color = Colors.DarkBlue;
+                    break;
+                default:
+                    color = Colors.Black;
+                    break;
+            }
+            return new SolidColorBrush(color);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
