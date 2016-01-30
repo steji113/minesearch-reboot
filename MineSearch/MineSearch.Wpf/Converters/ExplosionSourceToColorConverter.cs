4c7f873dfbbc5126372c36728da8cbe912cc57f0
mattmemmesheimer <m.memmesheimer@gmail.com>
2016-01-30 01:38:14 -0600

    Adding explosion source background to cell view.

---
+++ MineSearch/MineSearch.Wpf/Converters/ExplosionSourceToColorConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MineSearch.Wpf.Converters
+{
+    class ExplosionSourceToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool explosionSource = (bool) value;
+            Color color = explosionSource ? Colors.Red : Colors.LightGray;
+            return new SolidColorBrush(color);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
