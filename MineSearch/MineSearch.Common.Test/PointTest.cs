97fce7d90fd9957009552f17802ab9f89d2620ee
Stephen Daudell <s.daudell@boldwebservices.com>
2016-02-06 23:55:57 -0600

    Improved test coverage for Point.

--- MineSearch/MineSearch.Common.Test/PointTest.cs
+++ MineSearch/MineSearch.Common.Test/PointTest.cs
@@ -46,5 +46,58 @@ namespace MineSearch.Common.Test
                 }
             }
         }
+
+        [TestMethod]
+        public void TestEquality()
+        {
+            // Setup.
+            var p1 = new Point(1, 0);
+            var p2 = new Point(1, 0);
+            var p3 = new Point(0, 1);
+
+            // Dissimilar objects.
+            var s = "Test";
+            Assert.IsFalse(p1.Equals(s));
+
+            // Null object.
+            Assert.IsFalse(p1.Equals((object) null));
+
+            // Null point.
+            Assert.IsFalse(p1.Equals(null));
+
+            // Point as object equals.
+            Assert.IsTrue(p1.Equals((object) p2));
+            Assert.IsTrue(p2.Equals((object) p1));
+            Assert.IsFalse(p3.Equals((object) p1));
+
+            // Point as point equals.
+            Assert.IsTrue(p1.Equals(p2));
+            Assert.IsTrue(p2.Equals(p1));
+            Assert.IsFalse(p3.Equals(p1));
+
+            // Reference equals.
+            Assert.IsTrue(p1 == p1);
+
+            // Null checks.
+            Assert.IsFalse(null == p1);
+            Assert.IsFalse(p1 == null);
+
+            // Two different objects.
+            Assert.IsTrue(p1 == p2);
+
+            // Inequality operator.
+            Assert.IsTrue(p1 != p3);
+
+        }
+
+        [TestMethod]
+        public void TestHashCode()
+        {
+            int x = 2;
+            int y = 4;
+            var p = new Point(x, y);
+
+            Assert.AreEqual(x ^ y, p.GetHashCode());
+        }
     }
 }
