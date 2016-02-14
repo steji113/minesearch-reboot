6c5b69dc2fdfa95894c412a0011dc06f8885ef72
Stephen Daudell <s.daudell@boldwebservices.com>
2016-02-14 15:40:20 -0600

    Moved WPF ViewModel tests to their own folder.
    This is in preparation for adding other types of tests.

---
+++ MineSearch/MineSearch.Wpf.Test/ViewModelTests/SettingsViewModelTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MineSearch.Common;
+using MineSearch.Game;
+using MineSearch.Wpf.ViewModels;
+
+namespace MineSearch.Wpf.Test.ViewModelTests
+{
+    [TestClass]
+    public class SettingsViewModelTest
+    {
+        [TestMethod]
+        public void TestSave()
+        {
+            var gameSettings = new GameSettings(2, 2, 1, new RandomPointGenerator());
+            var viewModel = new SettingsViewModel
+            {
+                GameSettings = gameSettings
+            };
+            viewModel.SaveCommand.Execute(null);
+            Assert.IsTrue(viewModel.Saved);
+        }
+
+        [TestMethod]
+        public void TestCancel()
+        {
+            var gameSettings = new GameSettings(2, 2, 1, new RandomPointGenerator());
+            var viewModel = new SettingsViewModel
+            {
+                GameSettings = gameSettings
+            };
+            viewModel.CancelCommand.Execute(null);
+            Assert.IsFalse(viewModel.Saved);
+        }
+    }
+}
