1a15cf0a58edaede82d94399a055080c94389fcf
Stephen Daudell <s.daudell@boldwebservices.com>
2016-02-14 15:51:40 -0600

    Added a test for the InverseBooleanToVisibilityConverter.

---
+++ MineSearch/MineSearch.Wpf.Test/ConverterTests/InverseBooleanToVisibilityConverterTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MineSearch.Wpf.Converters;
+
+namespace MineSearch.Wpf.Test.ConverterTests
+{
+    [TestClass]
+    public class InverseBooleanToVisibilityConverterTest
+    {
+        [TestMethod]
+        public void TestVisibile()
+        {
+            var converter = new InverseBooleanToVisibilityConverter();
+
+            Assert.AreEqual(Visibility.Visible, converter.Convert(false, typeof(Visibility), null, null));
+        }
+
+        [TestMethod]
+        public void TestCollapsed()
+        {
+            var converter = new InverseBooleanToVisibilityConverter();
+
+            Assert.AreEqual(Visibility.Collapsed, converter.Convert(true, typeof(Visibility), null, null));
+        }
+    }
+}
