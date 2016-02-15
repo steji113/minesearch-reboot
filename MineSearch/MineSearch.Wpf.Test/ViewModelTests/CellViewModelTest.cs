246437d8f62d854a0b4bafc753e1d07e4c079018
mattmemmesheimer <m.memmesheimer@gmail.com>
2016-02-15 00:01:58 -0600

    Adding test for marking cells as questionable via the flag command.

--- MineSearch/MineSearch.Wpf.Test/ViewModelTests/CellViewModelTest.cs
+++ MineSearch/MineSearch.Wpf.Test/ViewModelTests/CellViewModelTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MineSearch.Common;
 using MineSearch.Game;
 using MineSearch.Wpf.ViewModels;
@@ -9,23 +10,56 @@ namespace MineSearch.Wpf.Test.ViewModelTests
     [TestClass]
     public class CellViewModelTest
     {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _generator = new RandomPointGenerator();
+            _gameSettings = new GameSettings(4, 4, 4, _generator);
+            _gameViewModel = new MineSearchGameViewModel(_gameSettings);
+        }
 
         [TestMethod]
         public void TestFlagCommand()
         {
-            var mockGameViewModel = new Mock<IMineSearchGameViewModel>();
-            var mockGame = new Mock<IMineSearchGame>();
-            var mockCell = new Mock<ICell>();
+            // Grab the first view model
+            var cellViewModel = _gameViewModel.CellViewModels[0].First();
+            var cell = cellViewModel.Cell;
 
-            mockGame.Setup(g => g.FlagCell(It.IsAny<Point>()));
-            mockGameViewModel.SetupGet(vm => vm.Game).Returns(mockGame.Object);
-            mockGameViewModel.SetupGet(vm => vm.GameActive).Returns(true);
-
-            // Create the view model and execute the flag command
-            var cellViewModel = new CellViewModel(mockGameViewModel.Object, mockCell.Object);
+            // Execute the flag command to mark the cell as flagged
             cellViewModel.FlagCommand.Execute(null);
+            Assert.IsTrue(cell.Flagged);
+            Assert.IsFalse(cell.Questionable);
 
-            mockGame.Verify(g => g.FlagCell(It.IsAny<Point>()), Times.Once());
+            // Execute the flag command a second time to clear the cell
+            cellViewModel.FlagCommand.Execute(null);
+            Assert.IsFalse(cell.Flagged);
+            Assert.IsFalse(cell.Questionable);
+        }
+
+        [TestMethod]
+        public void TestFlagCommandQuestionableEnabled()
+        {
+            _gameSettings.UseQuestionableState = true;
+            _gameViewModel = new MineSearchGameViewModel(_gameSettings);
+
+            // Grab the first view model
+            var cellViewModel = _gameViewModel.CellViewModels[0].First();
+            var cell = cellViewModel.Cell;
+
+            // Execute the flag command to mark the cell as flagged
+            cellViewModel.FlagCommand.Execute(null);
+            Assert.IsTrue(cell.Flagged);
+            Assert.IsFalse(cell.Questionable);
+
+            // Execute the flag command a second time to mark the cell as questionable
+            cellViewModel.FlagCommand.Execute(null);
+            Assert.IsTrue(cell.Questionable);
+            Assert.IsFalse(cell.Flagged);
+
+            // Execute the flag command a third time to clear the cell
+            cellViewModel.FlagCommand.Execute(null);
+            Assert.IsFalse(cell.Flagged);
+            Assert.IsFalse(cell.Questionable);
         }
 
         [TestMethod]
@@ -45,6 +79,10 @@ namespace MineSearch.Wpf.Test.ViewModelTests
 
             mockGame.Verify(g => g.RevealCell(It.IsAny<Point>()), Times.Once());
         }
+
+        private IGameSettings _gameSettings;
+        private IMineSearchGameViewModel _gameViewModel;
+        private IPointGenerator _generator;
 
     }
 }
